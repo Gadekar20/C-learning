018ded3ac7c2cfa9ffa7da9b727e0206439d4082
Shruti G <gadekarshrutika98@gmail.com>
2023-07-11 14:00:24 +0530

    Star Pattern

---
+++ pattern1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int i=1;
+    for(i=1;i<=n;i++)
+    {
+        int j=1;
+        for(j=1;j<=n;j++)
+        {
+            cout<<"*";
+            
+
+        }
+        cout<<endl;
+        
+
+    }
+}
